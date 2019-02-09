3247fc3c3be85b4055be2f030071500959f924ec
Matt Fan <fanma@ucla.edu>
2019-02-08 22:36:58 -0800

    Neuron inner class Connection

--- Neuron.py
+++ Neuron.py
@@ -5,21 +5,26 @@ import random
 # it just listens to its activations and does its thing
 
 class Neuron:
+    class Connection:
+        def __init__(self , connection):
+            self.connection = connection    # this is another neuron
+            self.weight = random.random() * 2 - 1   # Between -1 and 1
+
+        def print(self):
+            # purposely not printing the connected neuron
+            print("Weight: " + str(self.weight))
+
+
+
     def __init__(self):
         random.seed()
         self.bias = 0
         self.activation = 0
-
-        # This doesn't go here but save the code for later
-        self.weight = random.random() * 2 - 1   # Between -1 and 1
         
 
     def print(self):
         print("Bias: " + str(self.bias))
-
-        # Also doesn't go here
-        print("Weight: " + str(self.weight))
-
+        
 
     def sigmoid(self , input):
         e = 2.71828
